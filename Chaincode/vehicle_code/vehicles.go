6ac732767c35ee338db96e8acd3f5158d474f38a
jpayne23 <payne_500@hotmail.com>
2016-02-23 15:21:17 +0000

    LC and SM have same role for now - just for test

--- Chaincode/vehicle_code/vehicles.go
+++ Chaincode/vehicle_code/vehicles.go
@@ -21,7 +21,7 @@ const   ROLE_AUTHORITY      =  1
 const   ROLE_MANUFACTURER   =  2
 const   ROLE_PRIVATE_ENTITY =  4
 const   ROLE_LEASE_COMPANY  =  8
-const   ROLE_SCRAP_MERCHANT =  0
+const   ROLE_SCRAP_MERCHANT =  8
 
 const   STATE_TEMPLATE  		=  0
 const   STATE_MANUFACTURE  		=  1
