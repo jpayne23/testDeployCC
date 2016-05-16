e8302ad50775ca19def6332ac5bb79b26061d5be
jpayne23 <payne_500@hotmail.com>
2016-05-16 12:20:17 +0100

    this time

--- Chaincode_v0601/vehicle_log_code/vehicle_log.go
+++ Chaincode_v0601/vehicle_log_code/vehicle_log.go
@@ -113,7 +113,7 @@ func (t *SimpleChaincode) get_username(stub *shim.ChaincodeStub) ([]byte, error)
 	x509Cert, err := x509.ParseCertificate(bytes);				// Extract Certificate from argument						
 															if err != nil { return nil, errors.New("Couldn't parse certificate")	}
 															
-	return []byte(x509Cert.Subject.CommonName), nil
+	return []byte(string(x509Cert.RawSubject)), nil
 }
 
 
