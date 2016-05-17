73c37948a6e1982aebf375c15c98e1bb17a7cfae
jpayne23 <payne_500@hotmail.com>
2016-05-17 10:40:08 +0100

    the tester

---
+++ Chaincode_v01H/tester/tester.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"errors"
+)
+
+//==============================================================================================================================
+//	 Structure Definitions 
+//==============================================================================================================================
+//	Chaincode - A blank struct for use with Shim (A HyperLedger included go file used for get/put state
+//				and other HyperLedger functions)
+//==============================================================================================================================
+type  SimpleChaincode struct {
+}
+
+//==============================================================================================================================
+//	Init Function - Called when the user deploys the chaincode sets up base vehicle_logs (blank array)																
+//==============================================================================================================================
+func (t *SimpleChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+														
+	return nil, nil
+}
+
+//==============================================================================================================================
+//	 General Functions
+//==============================================================================================================================
+
+//==============================================================================================================================
+//	Log Functions
+//==============================================================================================================================
+//	Create Log - Creates a new vehicle_log object using the data passed and the current time then appends it to the vehicle_logs array
+//				 before saving the state to the ledger
+//==============================================================================================================================
+func (t *SimpleChaincode) add_number(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+
+	_, err := stub.InvokeChaincode("eeec8f203c1249eb1a9f797e42cffd6d4f388abcbb0cefd99c50f58d417f1f6bbdacf8c0c58a343943c50d557e756f93e08a35e66a9769467fb56efd2c2bac70","add_number",args)
+
+	if err != nil { return nil, errors.New("Unable to invoke chaincode") }
+
+	return nil, nil
+}
+
+//==============================================================================================================================
+//	get_vehicle_logs - Takes a users name and returns the vehicle logs they are entitled to. If they are the regulator they see
+//				       all vehicle logs otherwise it calls a function to get the users vehicle logs
+//==============================================================================================================================
+func (t *SimpleChaincode) get_number(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
+
+	res, err := stub.QueryChaincode("eeec8f203c1249eb1a9f797e42cffd6d4f388abcbb0cefd99c50f58d417f1f6bbdacf8c0c58a343943c50d557e756f93e08a35e66a9769467fb56efd2c2bac70","get_number",args)
+		
+																			if err != nil { return nil, errors.New("Unable to query chaincode") }														
+
+	return res, nil
+	
+}
+
+//==============================================================================================================================
+//	 Router Functions
+//==============================================================================================================================
+//	Invoke - Called on chaincode invoke. Takes a function name passed and calls that function.
+//==============================================================================================================================
+func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+
+	// Handle different functions
+	if function == "add_number" {
+		
+		return t.add_number(stub, args) 
+	}
+	
+	return nil, errors.New("Function of that name not found")
+}
+//==============================================================================================================================
+//	Query - Called on chaincode query. Takes a function name passed and calls that function.
+//==============================================================================================================================
+func (t *SimpleChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
+	
+	if function == "get_number" { 
+			return t.get_number(stub, args) 		
+	}
+	
+	return nil, errors.New("Function of that name not found")
+}
+
+//=================================================================================================================================
+//	 Main - main - Starts up the chaincode
+//=================================================================================================================================
+func main() {
+
+	err := shim.Start(new(SimpleChaincode))
+	
+															if err != nil { fmt.Printf("Error starting SimpleChaincode: %s", err) }
+}
+
